66a02bcb6f05e47a9169ccea0407c3242b415995
Maxx016 <djordjevic_marko@hotmail.com>
2024-03-20 11:46:51 +0100

    updated to use SQLAlchemy ORM models

--- py/02-labapp/labapp.py
+++ py/02-labapp/labapp.py
@@ -1,10 +1,11 @@
-import typer
-from sqlalchemy import create_engine, text
 import os
 import uuid
 from datetime import datetime
-from typing_extensions import Annotated
 import pandas as pd
+import typer
+from sqlalchemy import create_engine, DateTime, Float, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, Session, mapped_column
+from typing_extensions import Annotated
 
 app = typer.Typer()
 
@@ -12,125 +13,123 @@ dbPath = os.path.join(os.getcwd(), 'database.db')
 
 engine = create_engine(f'sqlite:///{dbPath}')
 
-conn = engine.connect()
 
-conn.execute(text("""
-    CREATE TABLE IF NOT EXISTS merenje (
-    id VARCHAR(36) NOT NULL PRIMARY KEY,
-    naziv_merenja VARCHAR(100) NOT NULL,
-    vreme_merenja DATETIME NOT NULL,
-    vrednost_merenja DOUBLE NOT NULL,
-    vreme_unosa DATETIME NOT NULL,
-    vreme_promene DATETIME NOT NULL
-    )
-"""))
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Merenje(Base):
+    __tablename__ = "merenje"
+
+    id: Mapped[str] = mapped_column(String(36), primary_key=True, nullable=False)
+    naziv_merenja: Mapped[str] = mapped_column(String(100), nullable=False)
+    vreme_merenja: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    vrednost_merenja: Mapped[float] = mapped_column(Float(precision=53), nullable=False)
+    vreme_unosa: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    vreme_promene: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+
+
+Base.metadata.create_all(engine)
 
 
 @app.command()
 def unos(naziv_merenja: Annotated[str, typer.Argument()],
-    vreme_merenja: Annotated[datetime, typer.Argument()],
-    vrednost_merenja: Annotated[float, typer.Argument()]
-):
-    conn.execute(text(f"""
-        INSERT INTO merenje (
-            id,
-            naziv_merenja,
-            vreme_merenja,
-            vrednost_merenja,
-            vreme_unosa,
-            vreme_promene)
-        VALUES (
-            '{uuid.uuid4()}',
-            '{naziv_merenja}',
-            '{vreme_merenja}',
-            {vrednost_merenja},
-            '{str(datetime.now().replace(microsecond=0))}',
-            '{str(datetime.now().replace(microsecond=0))}'
+         vreme_merenja: Annotated[datetime, typer.Argument()],
+         vrednost_merenja: Annotated[float, typer.Argument()]
+         ):
+    with Session(engine) as session:
+        formatted_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        timestamp = datetime.strptime(formatted_datetime, '%Y-%m-%d %H:%M:%S')
+
+        novo_merenje = Merenje(
+            id=str(uuid.uuid4()),
+            naziv_merenja=naziv_merenja,
+            vreme_merenja=vreme_merenja,
+            vrednost_merenja=vrednost_merenja,
+            vreme_unosa=timestamp,
+            vreme_promene=timestamp
         )
-    """))
-    conn.commit()
+
+        session.add(novo_merenje)
+        session.commit()
+
     print("Uspesno ste uneli merenje.")
 
 
 @app.command()
 def pregled(
-    id: Annotated[uuid.UUID, typer.Option(help="uuid4 merenja")] = None,
-    naziv: Annotated[str, typer.Option(help="naziv merenja")] = "",
-    vreme: Annotated[datetime, typer.Option(help="vreme merenja")] = None,
-    vrednost: Annotated[float, typer.Option(help="vrednost merenja")] = False,
-    vreme_unosa: Annotated[datetime, typer.Option(help="vreme unosa merenja")] = None,
-    limit: Annotated[int, typer.Option(help="limit prikaza rezultata")] = 10,
-    offset: Annotated[int, typer.Option(help="redni broj reda rezultata od kojeg pocinje listanje rezultata")] = 0
+        id: Annotated[uuid.UUID, typer.Option(help="uuid4 merenja")] = None,
+        naziv: Annotated[str, typer.Option(help="naziv merenja")] = "",
+        vreme: Annotated[datetime, typer.Option(help="vreme merenja")] = None,
+        vrednost: Annotated[float, typer.Option(help="vrednost merenja")] = False,
+        vreme_unosa: Annotated[datetime, typer.Option(help="vreme unosa merenja")] = None,
+        limit: Annotated[int, typer.Option(help="limit prikaza rezultata")] = 10,
+        offset: Annotated[int, typer.Option(help="redni broj reda rezultata od kojeg pocinje listanje rezultata")] = 0
 ):
-    options = [
-        ("id", id),
-        ("naziv_merenja", naziv),
-        ("vreme_merenja", vreme),
-        ("vrednost_merenja", vrednost),
-        ("vreme_unosa", vreme_unosa),
-    ]
+    with Session(engine) as session:
+        query = session.query(Merenje)
 
-    query = "SELECT * FROM MERENJE"
-    queryString = ""
+        if id:
+            query = query.filter(Merenje.id == id)
+        if naziv:
+            query = query.filter(Merenje.naziv_merenja == naziv)
+        if vreme:
+            query = query.filter(Merenje.vreme_merenja == vreme)
+        if vrednost:
+            query = query.filter(Merenje.vrednost_merenja == vrednost)
+        if vreme_unosa:
+            query = query.filter(Merenje.vreme_unosa == vreme_unosa)
 
-    for k, v in options:
-        if v:
-            if not queryString:
-                queryString = f" WHERE {k} = '{v}'"
-            else:
-                queryString += f" AND {k} = '{v}'"
+        query = query.offset(offset).limit(limit)
 
-    query += queryString
-    query += f" LIMIT {offset}, {limit}"
-    result = conn.execute(text(query))
-    df = pd.DataFrame(result)
-    if not df.empty:
-        print(df)
+        df = pd.read_sql(query.statement, session.bind)
+
+        if not df.empty:
+            print(df)
 
 
 @app.command()
 def brisanje(id: Annotated[uuid.UUID, typer.Argument(help="id merenja za brisanje")]):
-    if conn.execute(text(f"DELETE FROM merenje WHERE id = '{id}'")).rowcount > 0:
-        conn.commit()
-        print(f"Uspesno obrisano merenje id {id}!")
-    else:
-        print(f"Merenje nije pronadjeno! id: {id}")
+    with Session(engine) as session:
+        to_delete = session.query(Merenje).filter(Merenje.id == (str(id))).first()
+
+        if to_delete:
+            session.delete(to_delete)
+            session.commit()
+
+            print(f"Uspesno obrisano merenje id {id}!")
+        else:
+            print(f"Merenje nije pronadjeno! id: {id}")
 
 
 @app.command()
 def promena(
-    id: Annotated[uuid.UUID, typer.Argument(help="id merenja za promenu")] = None,
-    naziv: Annotated[str, typer.Option(help="novi naziv merenja")] = "",
-    vreme: Annotated[datetime, typer.Option(help="novo vreme merenja")] = None,
-    vrednost: Annotated[float, typer.Option(help="nova vrednost merenja")] = False
+        id: Annotated[uuid.UUID, typer.Argument(help="id merenja za promenu")] = None,
+        naziv: Annotated[str, typer.Option(help="novi naziv merenja")] = "",
+        vreme: Annotated[datetime, typer.Option(help="novo vreme merenja")] = None,
+        vrednost: Annotated[float, typer.Option(help="nova vrednost merenja")] = False
 ):
     if not any([naziv, vreme, vrednost]):
         print("Morate izabrati neku od opcija! (brisanje --help)")
         return
 
-    options = [
-      ("naziv_merenja", naziv),
-      ("vreme_merenja", vreme),
-      ("vrednost_merenja", vrednost)
-    ]
+    with Session(engine) as session:
+        to_update = session.query(Merenje).filter(Merenje.id == str(id)).first()
 
-    queryString = ""
-    for k, v in options:
-        if v:
-            if not queryString:
-                queryString += f" {k} = '{v}'"
-            else:
-                queryString += f", {k} = '{v}'"
+        if to_update:
+            if naziv:
+                to_update.naziv_merenja = naziv
+            if vreme:
+                to_update.vreme_merenja = vreme
+            if vrednost:
+                to_update.vrednost_merenja = vrednost
 
-    query = "UPDATE merenje SET"
-    query += queryString
-    query += f" WHERE id = '{id}'"
+            session.commit()
+            print(f"Uspesno izmenjeno merenje id {id}")
 
-    if conn.execute(text(query)).rowcount > 0:
-        conn.commit()
-        print(f"Uspesno izmenjeno merenje id {id}")
-    else:
-        print(f"Merenje nije pronadjeno! id: {id}")
+        else:
+            print(f"Merenje nije pronadjeno! id: {id}")
 
 
 if __name__ == "__main__":
